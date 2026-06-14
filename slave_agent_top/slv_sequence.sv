3a3e17f633a6cf1fe3e27cc552929af32441f422
Uma Bharathi Takkella <157229907+Umabharathi30@users.noreply.github.com>
2026-06-14 12:08:01 +0530

    Update and rename apb_sequence.sv to slv_sequence.sv

--- slave_agent_top/slv_sequence.sv
+++ slave_agent_top/slv_sequence.sv
@@ -1,8 +1,8 @@
-class apb_sequence extends uvm_sequence#(src_xtn);
+
+class slv_sequence extends uvm_sequence#(src_xtn);
         `uvm_object_utils(slv_sequence)
 
 function new(string name="slv_sequence");
         super.new(name);
 endfunction
 endclass
-
