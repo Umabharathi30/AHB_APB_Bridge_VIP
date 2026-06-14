b08ec4f5815036a8e4d4f72d3713b17b70b97749
Uma Bharathi Takkella <157229907+Umabharathi30@users.noreply.github.com>
2026-06-14 11:47:53 +0530

    Update and rename ahb_sequencer.sv to src_sequencer.sv

--- master_agent_top/ahb_sequencer.sv
+++
@@ -1,9 +0,0 @@
-class ahb_sequencer extends uvm_sequencer#(ahb_trans);
-
-   `uvm_component_utils(ahb_sequencer);
-
-function new(string name ="ahb_sequencer",uvm_component parent);
-   super.new(name,parent);
-endfunction:new
-
-endclass:ahb_sequencer
