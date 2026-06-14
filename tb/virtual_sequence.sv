bcb6ed226e8edea51357bb63707bc3d2211778b5
Uma Bharathi Takkella <157229907+Umabharathi30@users.noreply.github.com>
2026-06-14 12:18:14 +0530

    Create virtual_sequence.sv

---
+++ tb/virtual_sequence.sv
@@ -0,0 +1,9 @@
+class virtual_sequence extends uvm_sequence#(uvm_sequence_item);
+        `uvm_object_utils(virtual_sequence)
+
+        virtual_sequencer v_sequencer;
+
+function new(string name="virtual_sequence");
+        super.new(name);
+endfunction
+endclass
