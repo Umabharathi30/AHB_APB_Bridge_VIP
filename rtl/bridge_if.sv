35a2de88a065b2caf6f7bc91bb5cab1aeffa8307
Uma Bharathi Takkella <157229907+Umabharathi30@users.noreply.github.com>
2026-06-14 11:55:38 +0530

    Update bridge_if.sv

--- rtl/bridge_if.sv
+++ rtl/bridge_if.sv
@@ -1,80 +1,93 @@
-`include "definitions.v"
+interface bridge_if(input bit clock );
 
 
-interface  bridge_if(input bit clock);
 
 
-           logic  Hresetn;
-           logic[1:0] Htrans;
-	   logic[2:0]Hsize;
-	   logic Hreadyin;
-           logic[2:0] Hburst;
-	   logic[`WIDTH-1:0] Hwdata; 
-	   logic[`WIDTH-1:0] Haddr;
-	   logic Hwrite;
-           logic[`WIDTH-1:0] Prdata;
-	   logic[`WIDTH-1:0] Hrdata;
-	   logic[1:0] Hresp;
-	   logic Hreadyout;
-	   logic[`SLAVES-1:0] Pselx;
-	   logic Pwrite;
-	   logic Penable; 
-	   logic[`WIDTH-1:0] Paddr;
-	   logic[`WIDTH-1:0] Pwdata;
+        //inputs
+        logic Hresetn;
+        logic [1:0] Htrans;
+        logic [2:0] Hsize;
+        logic Hready_in;
+        logic [31:0] Hwdata;
+        logic [31:0] Haddr;
+        logic Hwrite;
+        logic [31:0]prdata;
+        logic [31:0] Hrdata;
+        logic [1:0] Hresp;
+        logic Hready_out;
+//      logic Hselapbif;
+        logic [3:0]pselx;
+        logic pwrite;
+        logic penable;
+        logic Hburst;
+        logic [31:0]paddr;
+        logic [31:0]pwdata;
 
 
-clocking AHB_DRV@(posedge clock);
 
-default input #1 output #1;
-input Hreadyout;
-output Hburst;
-output Hresetn,Hwrite,Hreadyin;
-output Hsize;
-output Htrans;
-output Hwdata,Haddr;
+clocking m_dr_cb@(posedge clock);
+        default input#1 output  #1;
+        output Hwdata;
+        output Hwrite;
+        output Hresetn;
+        output Htrans;
+        //output Hselapbif;
+        output Hready_in;
+        input Hresp;
+        input Hrdata;
+        output Haddr;
+        output Hsize;
+        input Hready_out;
+        output Hburst;
+        endclocking
+
+clocking m_mon_cb@(posedge clock);
+        default input#1 output  #1;
+
+        input Hresetn;
+        //input Hselapbif;
+        input Hresp;
+        input Hwrite;
+        input Htrans;
+        input Hready_in;
+        input Hready_out;
+        input Hrdata;
+        input Haddr;
+        input Hsize;
+        input Hwdata;
+        input Hburst;
+        endclocking
+
+clocking s_dr_cb @(posedge clock);
+        default input #1 output #1;
+
+        input  penable;
+        input pwrite;
+        input pwdata;
+        input pselx;
+        output prdata;
+        input paddr;
+
 
 endclocking
 
-clocking AHB_MON@(posedge clock);
+clocking s_mon_cb @(posedge clock);
+        default input #1 output #1;
 
-default input #1 output #1;
-
-input Hreadyout;
-input Hwdata,Haddr,Hrdata;
-input Hwrite,Htrans,Hreadyin;
-input Hburst,Hsize;
-input Hresp;
-
-endclocking 
-
- 
-
-clocking  APB_DRV@(posedge clock);
-
-default input #1 output #1;
-
-input Pselx;
-input Pwrite,Penable;
-input Pwdata,Paddr;
-output Prdata;
-
+        input prdata;
+        input pselx;
+        input pwrite;
+        input  penable;
+        input pwdata;
+        input paddr;
+        //input Hrdata;
+        //input Hresp;
 endclocking
 
-clocking APB_MON@(posedge clock);
 
-default input #1 output #1;
-
-input Prdata,Pwdata,Pwrite,Penable,Pselx,Paddr;
-
-endclocking
-
-modport AHB_DRV_MP(clocking AHB_DRV);
-
-modport AHB_MON_MP(clocking AHB_MON);
-
-modport APB_DRV_MP(clocking APB_DRV);
-
-modport APB_MON_MP(clocking APB_MON);
-
+        modport MSDR_MP(clocking m_dr_cb);
+        modport MSMON_MP(clocking m_mon_cb);
+        modport SDR_MP(clocking s_dr_cb);
+        modport SMON_MP(clocking s_mon_cb);
 
 endinterface
